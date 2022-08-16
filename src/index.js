a8e7c6bcd7b03277b20dfd4351873cfb36e15d73
rktdnjs <rktdnjs@naver.com>
2022-08-16 18:28:35 +0900

    마이페이지-서비스 틀

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 // axios withCredentials 전역설정, refreshToken 쿠키를 주고받기 위함
 axios.defaults.withCredentials = true;
 
--- src/pages/mypage/service.js
+++ src/pages/mypage/service.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 import '../../styles/section.css'
+import '../../styles/service.css'
+import Nav from 'react-bootstrap/Nav';
 
 const Service = () => {
+  // 서비스 기본 값 FAQ
+
   return (
     <div>
         <section>
           <div className="section__main">
-            <p>고객센터!</p>
-          </div>
+            <div className="section__mypage__service--title">SERVICE</div>
+            <div className="section__mypage__service--content">
+            <Nav fill variant="tabs" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="link-0" >FAQ</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="link-1" >1:1문의</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="link-2" >내 문의내역</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </div>
+          </div> 
         </section>
     </div>
   )
