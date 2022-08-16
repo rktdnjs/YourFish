5a0fb3554ba9d84b263fd77a6d05360f74779f44
rktdnjs <rktdnjs@naver.com>
2022-08-16 18:03:12 +0900

    마이페이지 - 위시리스트 'UI'만 구현

--- src/pages/mypage/wishlist.js
+++ src/pages/mypage/wishlist.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import '../../styles/section.css'
+import '../../styles/wishlist.css'
+import itemsample from '../../images/mypage/itemsample.png'
+import likebtn from '../../images/mypage/likebtn.png'
 
 const Wishlist = () => {
   return (
     <div>
         <section>
           <div className="section__main">
-            <p>찜 목록!</p>
+            <div className="section__mypage__wishlist--title">WISHLIST</div>
+            <div className='section__mypage__wishlist--contents'>
+              <img className='section__mypage__wishlist--contents--like' src={likebtn}/>
+              <div className='section__mypage__wishlist--contents--text'>찜한 상품 1개</div>
+              <img className='section__mypage__wishlist--contents--sample' src={itemsample}/>
+            </div>
           </div>
         </section>
     </div>
